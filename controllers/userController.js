f94c1f8fe8822a914d691e7ae9f78f04a0c8d14f
suraj.be <suraj.be@media.net>
2023-01-23 16:41:40 +0530

    able to register user with hashed password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,41 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcrypt')
+const User = require('../models/userModel')
 
 /**
  * @desc Register a user
  * @route POST /api/users/register
  * @access public
 */
-const registerUser = asyncHandler((req, res) => {
-  res.json({ message: 'Register the user' })
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body
+  if (!username || !email || !password) {
+    res.status(400)
+    throw new Error('All fields are mandatory')
+  }
+
+  const userAvailable = await User.findOne({ email })
+  if (userAvailable) {
+    res.status(400)
+    throw new Error('User Already Registered')
+  }
+
+  /* Hash Password */
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword
+  })
+
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email })
+  } else {
+    res.status(400)
+    throw new Error('User data is not valid')
+  }
+
+  res.json({ message: 'User Registered' })
 })
 
 /**
@@ -14,7 +43,7 @@ const registerUser = asyncHandler((req, res) => {
  * @route POST /api/users/login
  * @access public
 */
-const loginUser = asyncHandler((req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.json({ message: 'User logged in' })
 })
 
@@ -23,7 +52,7 @@ const loginUser = asyncHandler((req, res) => {
  * @route GET /api/users/current
  * @access private
 */
-const currentUser = asyncHandler((req, res) => {
+const currentUser = asyncHandler(async (req, res) => {
   res.json({ message: 'Current user information' })
 })
 
