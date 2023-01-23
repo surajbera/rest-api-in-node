62f27f8bb158740e9f3ea51ce5182bfd473fbf78
suraj.be <suraj.be@media.net>
2023-01-23 20:24:11 +0530

    dummy commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,8 @@ const router = express.Router()
 const { registerUser, loginUser, currentUser } = require('../controllers/userController')
 const validateToken = require('../middleware/validateTokenHandler')
 
-
 router.post('/register', registerUser)
-
 router.post('/login', loginUser)
-
 router.get('/current', validateToken, currentUser)
 
 module.exports = router
