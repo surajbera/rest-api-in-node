6a1548336503c7012f0b11c6f45a1c92f5de24f7
suraj.be <suraj.be@media.net>
2023-01-23 16:56:04 +0530

    Sent an accesstoken as a response on successfull login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 
 /**
@@ -44,6 +45,36 @@ const registerUser = asyncHandler(async (req, res) => {
  * @access public
 */
 const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    res.status(400)
+    throw new Error('All fields are mandatory')
+  }
+  const user = await User.findOne({ email })
+
+  /* Compare password with the hashed password */
+  /* 
+    password - password given while logging in
+    user.password - Hashed password stored in the database 
+  */
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user._id
+        }
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: '1m' }
+    )
+    res.status(200).json({ accessToken })
+  } else {
+    res.status(401)
+    throw new Error('Email or password is not valid')
+  }
+
   res.json({ message: 'User logged in' })
 })
 
